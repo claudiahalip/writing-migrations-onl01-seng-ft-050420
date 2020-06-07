6100c17f87475ed39060f40734133c3019141d9a
claudia v <claudia4ro@yahoo.com>
2020-06-07 02:24:41 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class ChangeDatatypeForBirthdate <ActiveRecord::Migration[5.2]
   
   def change
-    change_column(:students, :birthdate, :datetime ) 
+    change_column(:students, :birthdate, :string, :datetime ) 
   end 
 end 
