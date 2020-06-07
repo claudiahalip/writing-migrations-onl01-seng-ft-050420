c774b6f6dc57dd4d6eb8000c721c0a3e63291342
claudia v <claudia4ro@yahoo.com>
2020-06-07 02:32:30 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,13 +1,6 @@
 class CreateStudents < ActiveRecord::Migration[5.2]
   
-  def up
-  end
- 
-  def down
-  end
-  
-  
-  def change 
+ def change 
     create_table :students do |t|
       t.string :name 
     end
